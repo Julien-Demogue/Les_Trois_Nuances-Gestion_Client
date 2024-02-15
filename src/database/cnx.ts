80499298f124ae8d481cc759afe794d5fcee3911
Julien Demogue <julien.demogue@sfr.fr>
2024-02-15 18:36:09 +0100

    nwsdgn

--- src/database/cnx.ts
+++ src/database/cnx.ts
@@ -1,25 +1,7 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient,ServerApiVersion } from "mongodb";
 
-const mongoURL = 'mongodb://localhost:27017/gestion_client';
+const uri = process.env.MONGO_URL ?? "mongodb://localhost:27017"
 
-async function connect(): Promise<typeof MongoClient> {
-    try {
-        const client = new MongoClient(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
-        await client.connect();
-        return client;
-    }
-    catch (err) {
-        alert(err);
-    }
+export const client = new MongoClient(uri,{
+    serverApi:ServerApiVersion.v1
 }
-  
-async function close(client: typeof MongoClient): Promise<void> {
-    try{
-        await client.close();
-    }
-    catch (err){
-        alert(err);
-    }
-}
-
-export default { connect, close };
