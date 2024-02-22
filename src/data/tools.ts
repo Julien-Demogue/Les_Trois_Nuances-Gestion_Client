a0f21bab7e15ce1a9e515ea939ffbdf548ed7f4d
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2024-02-22 14:24:47 +0100

    remplissage de méthodes de vrification

--- src/data/tools.ts
+++ src/data/tools.ts
@@ -17,31 +17,38 @@ export default function verifyClientInfos(client:Client){
 }
 
 // Verifie la validite d'un email
-function verifyEmail(email:String){
-
+function verifyEmail(email:string): boolean{
+    const regex = RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+    return regex.test(email);
 }
 
 // verifie la validite d'une date
-function verifyDate(date:String){
+function verifyDate(date:string) : boolean{
+  const parsedDate = new Date(date);
 
+  if (isNaN(parsedDate.getTime())) {
+    return false;
+  }
+
+  return true;
 }
 
 // verifie la validite d'un code postal
-function verifyPostalCode(postalCode:Number){
-
+function verifyPostalCode(postalCode:number): boolean {
+    return postalCode.toString().length === 5 && !isNaN(postalCode);
 }
 
 // Formate un nom
-function formatName(name:String){
+function formatName(name:string){
 
 }
 
 // Formate une date
-function formatDate(date:String){
+function formatDate(date:string){
 
 }
 
 // Calcule l'age a partire d'une date de naissance
-function calculateAge(birthday:String){
+function calculateAge(birthday:string){
 
 }
